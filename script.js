7dc402e47c29a845c17de1302ec6d10f58b240e6
Orzoon Kunwar <orzoonkunwar7.ak@gmail.com>
2018-09-23 15:39:56 +1000

    re-do

---
+++ script.js
@@ -0,0 +1,11 @@
+// variables
+window.addEventListener("load", () => {
+let overlayCon = document.querySelector('.overlay');
+let secIntro = document.querySelector('.intro').offsetHeight;
+overlayCon.style.height = secIntro+'px';
+console.log(overlayCon.style.height)
+
+
+})
+
+//styling the overlay 
