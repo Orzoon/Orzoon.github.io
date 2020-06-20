f0ded1e76106b30938857a528b96016d22200ea8
Orzoon Kunwar <orzoonkunwar7.ak@gmail.com>
2020-06-20 19:05:01 +1000

    new change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("start")
-window.location.replace("https://orzoon-portfolio.herokuapp.com/")
+window.location.replace("www.kunarjun.com")
 // variables
 window.addEventListener("load", () => {
     let preloader = document.querySelector('.loader');
